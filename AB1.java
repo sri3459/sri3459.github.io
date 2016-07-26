673281694111bbb697ce25d4c022b56996329179
sri3459 <srikanthmadireddy44@gmail.com>
2016-07-26 23:23:23 +0530

    Add files via upload

---
+++ AB1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class AB
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a");
+int a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("number is even");
+}
+else
+{
+System.out.println("number is odd");
+}
+}
+}
