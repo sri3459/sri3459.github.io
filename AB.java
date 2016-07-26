731da73229154766163e6ce9deb280a27d3c3b1d
sri3459 <srikanthmadireddy44@gmail.com>
2016-07-26 23:13:52 +0530

    Add files via upload

---
+++ AB.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class AB
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a");
+int a=sc.nextInt();
+if(a==0)
+{
+System.out.println("number is zero");
+}
+else
+{
+if(a>0)
+{
+System.out.println("number is positive");
+}
+else
+{
+System.out.println("number is negative");
+}
+}
+}
+}
