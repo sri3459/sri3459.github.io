b0007c8f57d07614e8b2668e1e32498e56f068d3
sri3459 <srikanthmadireddy44@gmail.com>
2016-07-27 21:21:17 +0530

    Add files via upload

---
+++ V.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class V
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a,b,c");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if((a>b) && (a>c))
+{
+System.out.println("a is largest");
+}
+else
+{
+if(b>c)
+{
+System.out.println("b is largest");
+}
+else
+{
+System.out.println("c is largest");
+}
+}
+}
+}
