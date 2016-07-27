c6d5b6c54025fba7e2dd8deebad91812176baacd
sri3459 <srikanthmadireddy44@gmail.com>
2016-07-28 00:47:12 +0530

    Add files via upload

---
+++ S.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class S
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter m,n");
+int m=sc.nextInt();
+int n=sc.nextInt();
+System.out.println("power of number is "+(Math.pow(m,n)));
+}
+}
