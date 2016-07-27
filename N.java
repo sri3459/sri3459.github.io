4ef39f487e1b393dc750ad3b9c74e2724d40b7ff
sri3459 <srikanthmadireddy44@gmail.com>
2016-07-27 21:59:55 +0530

    Add files via upload

---
+++ N.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class N
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a");
+int a=sc.nextInt();
+if(a%4==0)
+{
+System.out.println("given year is leap year");
+}
+else
+{
+System.out.println("given year is not an leap year");
+}
+}
+}
